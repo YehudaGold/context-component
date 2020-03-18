99818184acfdeb39431461d875bdac6b5f653fac
Yehuda Goldberg <hvusvd@gmail.com>
2020-03-18 17:22:48 +0200

    clean to one file

--- src/lib/ContextComponent.jsx
+++ src/lib/ContextComponent.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import {createConnectFunction} from './createConnectFunction';
-import {createProviderComponent} from './createProviderComponent';
-// Import {createContext, getContext} from './ContextStore';
+import React, {useContext} from 'react';
+
+const defaultComponentFunctions = ['state', ...Object.keys(new React.Component())];
+
+const createProviderComponent = (Provider, contextClass) =>
+    class ProviderComponent extends contextClass {
+        constructor(props) {
+            super(props);
+
+            Object.keys(this)
+                .filter(componentFunction => !defaultComponentFunctions.includes(componentFunction))
+                .forEach((contextFunction) => {
+                    this.state[contextFunction] = this[contextFunction];
+                });
+        }
+
+        render() {
+            return (
+                <Provider value={this.state}>
+                    {this.props.children}
+                </Provider>
+            );
+        }
+    };
+
+const createConnectFunction = context =>
+    (mapContextToProps = () => {}) =>
+        WrappedComponent =>
+            // Named react component for displaying in the components tree
+            function ConnectComponent(props) {
+                const contextValue = useContext(context);
+                return <WrappedComponent {...mapContextToProps(contextValue)} {...props} />;
+            };
 
 
 export const createContext = (contextClass) => {
     const context = React.createContext();
 
-    const component = {
+    return {
+        context,
         connect: createConnectFunction(context),
         Consumer: context.Consumer,
-        context,
         Provider: createProviderComponent(context.Provider, contextClass)
     };
-
-    return component;
 };
--- src/lib/createConsumerComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const createConsumerComponent = Consumer =>
-    // Named react component for displaying in the components tree
-    function ConnectedComponent({children}) {
-        return (
-            <Consumer>{children}</Consumer>
-        );
-    }
