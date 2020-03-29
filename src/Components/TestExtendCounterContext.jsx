9f25420a2f59c8635ad5debe086047948431e277
Yehuda Goldberg <hvusvd@gmail.com>
2020-03-29 16:04:57 +0300

    added timeContext with muont/unmunt methods test

--- src/Components/TestExtendCounterContext.jsx
+++ src/Components/TestExtendCounterContext.jsx
@@ -6,7 +6,7 @@ import CounterConnect from './Counter/CounterConnect';
 const TestExtendCounterContext = () =>
     <ExtendCounterContext>
         <div className="consumers-list">
-            ExtendCounterContext - test contextComponent of inherent class have all inherent actions automatically
+            TestExtendCounterContext - test contextComponent of inherent class have all inherent actions automatically
             <CounterConnect />
         </div>
     </ExtendCounterContext>;
---
+++ src/Contexts/TimeContext.jsx
@@ -0,0 +1,31 @@
+import ContextComponent from '../lib';
+
+const intervalTime = 1000;
+
+export default class TimeContext extends ContextComponent {
+
+    state = {timeFromMount: null, timeOfRender: null}
+
+    componentDidMount() {
+        this.mountTime = Date.now();
+        this.timerID = setInterval(
+            () => this.tick(),
+            intervalTime
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            timeFromMount: Date.now() - this.mountTime
+        });
+    }
+
+
+    // Remove tick from the context actions by setting action manually
+    actions={}
+
+}
